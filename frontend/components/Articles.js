6c5ae1704c28e3e7c356e5ebfe5816c199f35877
Ladrillo <gabriel@cabrejas.net>
2022-02-22 19:34:47 +0100

    committing to "lecture"

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -12,23 +12,23 @@ export default function Articles(props) {
     getArticles()
   }, [])
 
-  if (spinner) return 'fetching articles...'
-
   return (
     <div className="articles">
       <h2>Articles</h2>
       {
-        articles.map(art => {
-          return (
-            <div className="article" key={art.article_id}>
-              <div>
-                <h3>{art.title}</h3>
-                <p>{art.text}</p>
-                <p>topic: {art.topic}</p>
+        spinner
+          ? 'Getting articles...'
+          : articles.map(art => {
+            return (
+              <div className="article" key={art.article_id}>
+                <div>
+                  <h3>{art.title}</h3>
+                  <p>{art.text}</p>
+                  <p>topic: {art.topic}</p>
+                </div>
               </div>
-            </div>
-          )
-        })
+            )
+          })
       }
     </div>
   )
