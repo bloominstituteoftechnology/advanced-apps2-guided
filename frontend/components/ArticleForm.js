7cb0b56c6da8750efafda508b4f9baeb3ec684b2
Ladrillo <gabriel@cabrejas.net>
2022-02-22 15:42:20 +0100

    ready

--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -4,16 +4,28 @@ const initialFormValues = { title: '', text: '', topic: '' }
 export default function ArticleForm(props) {
   const [values, setValues] = useState(initialFormValues)
 
+  const onChange = evt => {
+    const { id, value } = evt.target
+    setValues({ ...values, [id]: value })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+
+  }
+
   return (
-    <form id="form">
+    <form id="form" onSubmit={onSubmit}>
       <h2>Create Article</h2>
       <input
         maxLength={50}
+        onChange={onChange}
         placeholder="Enter title"
         id="title"
       />
       <textarea
         maxLength={200}
+        onChange={onChange}
         placeholder="Enter text"
         id="text"
       />
@@ -23,7 +35,7 @@ export default function ArticleForm(props) {
         <option value="React">React</option>
         <option value="Node">Node</option>
       </select>
-        <button id="submitArticle">Submit</button>
+      <button id="submitArticle">Submit</button>
     </form>
   )
 }
