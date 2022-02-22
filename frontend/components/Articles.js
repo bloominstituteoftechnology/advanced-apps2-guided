e83995a7349acf14c1178d9fe6e6fbe100a592fc
Ladrillo <gabriel@cabrejas.net>
2022-02-22 19:08:45 +0100

    committing to "lecture"

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Articles(props) {
   const { articles, getArticles } = props
+
+  useEffect(() => {
+    getArticles()
+  }, [])
 
   return (
     <div className="articles">
