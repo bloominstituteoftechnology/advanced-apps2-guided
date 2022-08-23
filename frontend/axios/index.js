e74e067417225eecc37f9b50fd3f19aded8d8558
Ladrillo <gabriel@cabrejas.net>
2022-08-23 19:28:15 +0200

    committing to "lecture"

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,11 @@
-// with auth
+import axios from 'axios'
+
+export default () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    headers: {
+      Authorization: token,
+    },
+  });
+};
