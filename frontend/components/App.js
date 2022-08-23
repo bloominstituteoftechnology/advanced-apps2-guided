8a0c5df4052edfff7aa39a492f3006171897e55f
Ladrillo <gabriel@cabrejas.net>
2022-08-23 19:00:48 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -32,7 +32,7 @@ export default function App() {
   }
 
   const getArticles = () => {
-    axios.get(articlesUrl)
+    axios.get(articlesUrl) // we are not providing token :(
       .then(res => {
         debugger
       })
